f18cd4cae4ae1e71ce2df9f02a3269e2c5f89840
cshannon3 <crssoccer3@gmail.com>
2019-03-19 23:32:39 -0400

    added bracket

--- app/docs/views.py
+++ app/docs/views.py
@@ -10,3 +10,20 @@ def index():
     :return:
     """
     return view('index.html')
+from flask import Blueprint, render_template as view
+
+docs = Blueprint('docs', __name__, static_folder='static', template_folder='templates')
+
+
+@docs.route('/')
+def index():
+    """
+    Show an index template
+    :return:
+    """
+    return view('index.html')
+
+
+@docs.route('/bracket')
+def bracket():
+    return view('bracket.html')
