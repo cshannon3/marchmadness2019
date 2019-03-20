a543c215835cafb149ba5d6e942eb51ea7fd8bd2
cshannon3 <crssoccer3@gmail.com>
2019-03-19 22:50:30 -0400

    structured app

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+
+
+# Initialize application
+app = Flask(__name__, static_folder=None)
+
+# # app configuration
+# app_settings = os.getenv(
+#     'APP_SETTINGS',
+#     'app.config.DevelopmentConfig'
+# )
+# app.config.from_object(app_settings)
+
+
+# Import the application views
+from app import views
+
+
+from app.docs.views import docs
+
+app.register_blueprint(docs)
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
-from flask import Flask
+# from flask import Flask
  
-app = Flask(__name__)
+# app = Flask(__name__)
  
-@app.route('/hello')
-def helloWorldHandler():
-    return 'Hello World from Flask!'
+# @app.route('/hello')
+# def helloWorldHandler():
+#     return 'Hello World from Flask!'
  
 
-@app.route('/bracket')
-def bracketHandler():
+# @app.route('/bracket')
+# def bracketHandler():
     
-    return 'Hello World from Flask!'
+#     return 'Hello World from Flask!'
  
-app.run(host='0.0.0.0', port= 8090)
+# app.run(host='0.0.0.0', port= 8090)
 
-# class GameModel:
-# class TeamModel:
-# class UserModel:
+# # class GameModel:
+# # class TeamModel:
+# # class UserModel:
