fec0d90a0a4299179db13983bb1ffefd0cf70a67
cshannon3 <crssoccer3@gmail.com>
2019-03-20 21:03:16 -0400

    starting to tie in scraper

--- app/docs/views.py
+++ app/docs/views.py
@@ -1,18 +1,13 @@
 from flask import Blueprint, render_template as view
+from bs4 import BeautifulSoup as soup
+import urllib.request
+
+
+
 
 docs = Blueprint('docs', __name__, static_folder='static', template_folder='templates')
 
 
-@docs.route('/')
-def index():
-    """
-    Show an index template
-    :return:
-    """
-    return view('index.html')
-from flask import Blueprint, render_template as view
-
-docs = Blueprint('docs', __name__, static_folder='static', template_folder='templates')
 
 
 @docs.route('/')
@@ -27,3 +22,20 @@ def index():
 @docs.route('/bracket')
 def bracket():
     return view('bracket.html')
+
+
+@docs.route('/listteams')
+def listteams():
+    teams = []
+    #points = 0
+    bracket = "http://fantasy.espn.com/tournament-challenge-bracket/2018/en/entry?entryID=5444662"
+    with urllib.request.urlopen(bracket) as response:
+        html = response.read()
+    mysoup = soup(html, 'html.parser')
+    gameid = 0
+    for gameid in range(64):
+        match = mysoup.find("div", attrs={'data-slotindex': str(gameid)})
+        teamname = match.find('span', attrs={'class': 'name'})
+        teams.append(teamname)
+     
+    return render_template('listteams.html' teams=teams)
